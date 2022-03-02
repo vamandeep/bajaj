6340e7264a888aa45dd3404ebf23fd918d0dfa87
vamandeep <58330787+vamandeep@users.noreply.github.com>
2022-03-02 14:15:35 +0530

    Add files via upload

---
+++ bajaj/endpoint.js
@@ -0,0 +1,32 @@
+var numericArr = [];
+var alphaArr = [];
+var model = {
+  is_success: true,
+  user_id: "vamandeep_11092001",
+  email: "vamandeep0164.cse19@chitkara.edu.in",
+  roll_number: "1910990164",
+  numbers: numericArr,
+  alphabets: alphaArr,
+};
+
+function sorter(arr) {
+  arr.forEach((i) => {
+    if (isNaN(parseInt(i))) alphaArr.push(i);
+    else numericArr.push(i);
+  });
+}
+
+module.exports = function (app) {
+  app.get("/", (req, res) => {
+    res.send("Please refer to /bhfl");
+  });
+
+  app.post("/bfhl", (req, res) => {
+    var data = req.body.data;
+
+    sorter(data);
+    res.json(model);
+    numericArr.length=0
+    alphaArr.length=0
+  });
+};
